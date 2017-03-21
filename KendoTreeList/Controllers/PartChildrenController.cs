e29da88f3d45a0302c0189a7f66d829cb3fc80a4
John Welty <john@weltyautomation.com>
2017-03-21 14:18:47 -0500

    ic

---
+++ KendoTreeList/Controllers/PartChildrenController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KendoTreeList.Models;
+
+namespace KendoTreeList.Controllers
+{
+    public class PartChildrenController : Controller
+    {
+        // GET: PaChildren
+        public JsonResult Index(int? id)
+        {
+            var data = new List<ChildData>
+            {
+                new ChildData
+                {
+                    Id = 1,
+                    PartNo = "Top Level",
+                    Description = "Top Level Part",
+                    HasChildren = true,
+                    ParentId = null
+                },
+                new ChildData
+                {
+                    Id = 2,
+                    PartNo = "Second Level",
+                    Description = "Second Level Part",
+                    HasChildren = true,
+                    ParentId = 1
+                },
+                new ChildData
+                {
+                    Id = 3,
+                    PartNo = "Third Level",
+                    Description = "Third Level Part",
+                    HasChildren = false,
+                    ParentId = 2
+                },
+            };
+
+            var children = data
+                .Where(c => c.ParentId == id);
+
+            var result = Json(children, JsonRequestBehavior.AllowGet);
+            return result;
+        }
+
+
+
+
+    }
+
+
+}
---
+++ KendoTreeList/Models/ChildData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KendoTreeList.Models
+{
+    public class ChildData
+    {
+        public int Id { get; set; }
+        public string PartNo { get; set; }
+        public string Description { get; set; }
+        public bool HasChildren { get; set; }
+        public int? ParentId { get; set; }
+    }
+}
